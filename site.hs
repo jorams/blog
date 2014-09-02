c3b39c99957364186160010c1cddb780cd0348df
Joram Schrijver <i@joram.io>
2014-09-02 19:06:43 +0200

    Initial infrastructure commit

---
+++ site.hs
@@ -0,0 +1,62 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+import           Text.Pandoc.Options
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "fonts/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "style.css" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandocCompilerWith defaultHakyllReaderOptions withToc
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let indexCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateCompiler
+  where
+    withToc = defaultHakyllWriterOptions
+        { writerTableOfContents = True
+        , writerTemplate =
+            "$if(toc)$<div id=\"TOC\"><h2>Table of Contents</h2>$toc$</div>\n$endif$$body$"
+        , writerStandalone = True
+        , writerReferenceLinks = False }
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%Y-%d-%m" `mappend`
+    defaultContext
+
+--------------------------------------------------------------------------------
+-- Useful pieces of code -- For reference
+--------------------------------------------------------------------------------
+-- match (fromList ["about.rst", "contact.markdown"]) $ do
+---- Aka: You can also match a specific list of things.
